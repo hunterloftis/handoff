2c91e4ec8eec8bd10e87722cb084c694af13e26c
Hunter Loftis <hunter@hunterloftis.com>
2014-11-27 00:40:44 -0700

    filling the correct boxes with purple

--- blit/blit.js
+++ blit/blit.js
@@ -2,9 +2,19 @@
 
   var VERTEX_SHADER = [
     'attribute vec2 a_position;',
+    'uniform vec2 u_resolution;',
 
     'void main() {',
-      'gl_Position = vec4(a_position, 0, 1);',
+      // convert the rectangle from pixels to 0.0 to 1.0
+      'vec2 zeroToOne = a_position / u_resolution;',
+
+      // convert from 0->1 to 0->2
+      'vec2 zeroToTwo = zeroToOne * 2.0;',
+
+      // convert from 0->2 to -1->+1 (clipspace)
+      'vec2 clipSpace = zeroToTwo - 1.0;',
+
+      'gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);',
     '}'
   ].join('\n');
 
@@ -21,12 +31,15 @@
     return shader;
   }
 
-  function initShaders(gl, vShader, fShader) {
+  function initShaders(gl, vShader, fShader, width, height) {
     var program = gl.createProgram();
     gl.attachShader(program, vShader);
     gl.attachShader(program, fShader);
     gl.linkProgram(program);
     gl.useProgram(program);
+
+    var resolutionLocation = gl.getUniformLocation(program, 'u_resolution');
+    gl.uniform2f(resolutionLocation, width, height);
 
     return gl.getAttribLocation(program, 'a_position');
   }
@@ -35,17 +48,17 @@
     var buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
 
-    var vertices = [
-      -1.0, -1.0,
-      1.0, -1.0,
-      -1.0,  1.0,
-      -1.0,  1.0,
-      1.0, -1.0,
-      1.0,  1.0
-    ];
+    // var vertices = [
+    //   10, 20,
+    //   80, 20,
+    //   10, 30,
+    //   10, 30,
+    //   80, 20,
+    //   80, 30
+    // ];
 
     gl.enableVertexAttribArray(posAttr);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
     gl.vertexAttribPointer(posAttr, 2, gl.FLOAT, false, 0, 0);
   }
 
@@ -57,13 +70,13 @@
 
     initGL(this.gl);
 
+    this.resize();
+
     this.vShader = createShader(this.gl, 'VERTEX_SHADER', VERTEX_SHADER);
     this.fShader = createShader(this.gl, 'FRAGMENT_SHADER', FRAGMENT_SHADER);
 
-    var posAttr = initShaders(this.gl, this.vShader, this.fShader);
+    var posAttr = initShaders(this.gl, this.vShader, this.fShader, this.width, this.height);
     initBuffers(this.gl, posAttr);
-
-    this.resize();
   }
 
   Surface.prototype.resize = function() {
@@ -132,7 +145,16 @@
   Sprite.prototype.blit = function(surface, x, y, frame) {
     //surface.ctx.drawImage(this.image, x, y);
     var gl = surface.gl;
+    var x1 = x;
+    var x2 = x + this.width;
+    var y1 = y;
+    var y2 = y + this.height;
 
+    var vertices = new Float32Array([
+      x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2
+    ]);
+
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
     gl.drawArrays(gl.TRIANGLES, 0, 6);
   };
 

260c612789d5b5002dd8ade11d11155bd6627fd3
Hunter Loftis <hunter@hunterloftis.com>
2014-11-28 22:36:45 -0700

    Hell yeah!

--- blit/blit.js
+++ blit/blit.js
@@ -2,6 +2,10 @@
 
   var VERTEX_SHADER = [
     'attribute vec2 a_position;',
+
+    'attribute vec2 a_texture;',
+    'varying vec2 v_texture;',
+
     'uniform vec2 u_resolution;',
 
     'void main() {',
@@ -15,45 +19,86 @@
       'vec2 clipSpace = zeroToTwo - 1.0;',
 
       'gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);',
+
+      // pass the texture coordinate to the fragment shader
+      'v_texture = a_texture;',
     '}'
   ].join('\n');
 
   var FRAGMENT_SHADER =[
+    'precision mediump float;',
+
+    // our texture
+    'uniform sampler2D u_image;',
+
+    // the texture coords passed in from the vertex shader
+    'varying vec2 v_texture;',
+
     'void main(void) {',
-      'gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);',
+      'gl_FragColor = texture2D(u_image, v_texture);',
     '}'
   ].join('\n');
 
-  function createShader(gl, type, source) {
-    var shader = gl.createShader(gl[type]);
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-    return shader;
-  }
+  // GL Utils
 
-  function initShaders(gl, vShader, fShader, width, height) {
+  function initGL(gl, width, height) {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.depthFunc(gl.LEQUAL);
+    gl.disable(gl.DEPTH_TEST);  // TODO: use this instead of y-sorting?
+    gl.disable(gl.CULL_FACE);
+    gl.enable(gl.BLEND);
+    gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, false);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+
+    // Load and compile fragment shader
+    var fShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fShader, FRAGMENT_SHADER);
+    gl.compileShader(fShader);
+    var compiled = gl.getShaderParameter(fShader, gl.COMPILE_STATUS);
+    if (!compiled) {
+      throw new Error('fragment shader error: ' + gl.getShaderInfoLog(fShader));
+    }
+
+    // Load and compile vertex shader
+    var vShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vShader, VERTEX_SHADER);
+    gl.compileShader(vShader);
+    var compiled = gl.getShaderParameter(vShader, gl.COMPILE_STATUS);
+    if (!compiled) {
+      throw new Error('vertex shader error: ' + gl.getShaderInfoLog(vShader));
+    }
+
+    // Create the shader program
     var program = gl.createProgram();
-    gl.attachShader(program, vShader);
     gl.attachShader(program, fShader);
+    gl.attachShader(program, vShader);
     gl.linkProgram(program);
     gl.useProgram(program);
 
+    // Link vertex position attribute from shader
+    var vertexPosition = gl.getAttribLocation(program, 'a_position');
+    gl.enableVertexAttribArray(vertexPosition);
+
+    // Link texture coordinate attribute from shader
+    var vertexTexture = gl.getAttribLocation(program, "a_texture");
+    gl.enableVertexAttribArray(vertexTexture);
+
+    // Provide the resolution (TODO: update on resize?)
     var resolutionLocation = gl.getUniformLocation(program, 'u_resolution');
     gl.uniform2f(resolutionLocation, width, height);
 
-    return gl.getAttribLocation(program, 'a_position');
-  }
-
-  function initBuffers(gl, posAttr) {
-    var buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);   // How is this working? what's this for?
-    gl.enableVertexAttribArray(posAttr);
-    gl.vertexAttribPointer(posAttr, 2, gl.FLOAT, false, 0, 0);
+    return {
+      position: vertexPosition,
+      texture: vertexTexture
+    };
   }
 
   function getGLContext(canvas, opts) {
     return canvas.getContext('webgl', opts) || canvas.getContext('experimental-webgl', opts);
   }
+
+  // Surface
 
   function Surface(canvas) {
     this.canvas = canvas;
@@ -61,15 +106,8 @@
     this.height = 0;
     this.gl = getGLContext(canvas, { alpha: false, premultipliedAlpha: false });
 
-    initGL(this.gl);
-
     this.resize();
-
-    this.vShader = createShader(this.gl, 'VERTEX_SHADER', VERTEX_SHADER);
-    this.fShader = createShader(this.gl, 'FRAGMENT_SHADER', FRAGMENT_SHADER);
-
-    var posAttr = initShaders(this.gl, this.vShader, this.fShader, this.width, this.height);
-    initBuffers(this.gl, posAttr);
+    this.attrs = initGL(this.gl, this.width, this.height);
   }
 
   Surface.prototype.resize = function() {
@@ -112,18 +150,10 @@
     };
   };
 
-  function initGL(gl) {
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    gl.depthFunc(gl.LEQUAL);
-    gl.disable(gl.DEPTH_TEST);  // TODO: use this instead of y-sorting?
-    gl.disable(gl.CULL_FACE);
-    gl.enable(gl.BLEND);
-    gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, false);
-    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-  }
+  // Sprite
 
-  function Sprite(url, width, height) {
+  function Sprite(surface, width, height, url) {
+    this.surface = surface;
     this.loaded = false;
     this.width = width;
     this.height = height;
@@ -133,25 +163,83 @@
   }
 
   Sprite.prototype._onLoad = function() {
+    var gl = this.surface.gl;
+    var image = this.image;
+
     this.loaded = true;
+
+    // Create a new texture and assign it as the active one
+    this.texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, this.texture);
+    //gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+
+    // Load in the image
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
+    // Setup scaling properties
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+    gl.generateMipmap(gl.TEXTURE_2D);
+
+    // Unbind the texture
+    gl.bindTexture(gl.TEXTURE_2D, null);
   };
 
   // TODO: enable depth sorting so you don't have to sort by y every frame?
   Sprite.prototype.blit = function(surface, x, y, frame) {
     var gl = surface.gl;
+    var vertexPosition = surface.attrs.position;
+    var vertexTexture = surface.attrs.texture;
+
+    // Create a new buffer for the vertices
+    var vertexBuffer = gl.createBuffer();
+
+    // Bind it as the current buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);   // How is this working? what's this for?
+
+    // Fill it with the vertex data
     var x1 = x;
     var x2 = x + this.width;
     var y1 = y;
     var y2 = y + this.height;
-
     var vertices = new Float32Array([
       x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2
     ]);
-
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    // Connect vertex buffer to shader's vertex position attribute
+    gl.vertexAttribPointer(vertexPosition, 2, gl.FLOAT, false, 0, 0);
+
+    // Create a new buffer for the texture
+    var textureBuffer = gl.createBuffer();
+
+    // Bind it as the current buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, textureBuffer);
+
+    // Fill it with the texture data
+    var textureCoords = new Float32Array([
+      0.0, 0.0,
+      1.0, 0.0,
+      0.0, 1.0,
+      0.0, 1.0,
+      1.0, 0.0,
+      1.0, 1.0
+    ]);
+    gl.bufferData(gl.ARRAY_BUFFER, textureCoords, gl.STATIC_DRAW);
+
+    // Connect texture buffer to shader's texture attribute
+    gl.vertexAttribPointer(vertexTexture, 2, gl.FLOAT, false, 0, 0);
+
+    // Set slot 0 as active texture
+    //gl.activeTexture(this.GL.TEXTURE0); // TODO: necessary?
+
+    // Load texture into memory
+    gl.bindTexture(gl.TEXTURE_2D, this.texture);
+
     gl.drawArrays(gl.TRIANGLES, 0, 6);
   };
 
+  // Export
 
   window.Blit = {
     Surface: Surface,
